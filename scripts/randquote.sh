670934206bcf91e19b9e8f615c084f9b1f20911b
Aaron Evan-Browning <aaevan@gmail.com>
2019-08-13 20:16:16 -0400

    Added outlines and random colors to holzer scripts

--- scripts/randquote.sh
+++ scripts/randquote.sh
@@ -1,2 +1,3 @@
-shuf -n 1 /home/aaron/scripts/survival_series.txt | fold -s -w 25 | sed 's/.$//' | osd_cat --pos=middle --align=center --font=-*-helvetica-bold-r-*-*-60-*-*-*-*-*-*-*
+COLOR=`~/scripts/rand_color.py`
+shuf -n 1 /home/aaron/scripts/survival_series.txt | fold -s -w 25 | sed 's/.$//' | osd_cat --pos=middle --align=center --outline=4 --font=-*-helvetica-bold-r-*-*-60-*-*-*-*-*-*-* --color=$COLOR
 
--- scripts/truisms.sh
+++ scripts/truisms.sh
@@ -1,2 +1,2 @@
-shuf -n 1 /home/aaron/scripts/truisms.txt | fold -s -w 25 | osd_cat --pos=middle --align=center --font=-*-helvetica-bold-r-*-*-60-*-*-*-*-*-*-*
+shuf -n 1 /home/aaron/scripts/truisms.txt | fold -s -w 25 | osd_cat --pos=middle --align=center --outline=4 --font=-*-helvetica-bold-r-*-*-60-*-*-*-*-*-*-*
 
