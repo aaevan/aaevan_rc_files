11abc56f127fe7aa31fc8fec240a0527d8522d18
Aaron Evan-Browning <aaevan@gmail.com>
2019-09-02 23:01:45 -0400

    Config files update for Mon Sep  2 23:01:45 EDT 2019

--- scripts/multi_keyer.sh
+++ scripts/multi_keyer.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-#key=Down
 key=$1
 delay=$2
 repeats=$3
 
+echo $key | osd_cat --pos=top --align=right --font=-*-helvetica-bold-r-*-*-30-*-*-*-*-*-*-* --offset=-4 -i 150 -d 1 -O 20 -c blue &
 for i in $(seq 1 $repeats)
 do
+        xdotool keydown --delay 20 $key
         xdotool keydown --delay $delay $key
         xdotool keyup --delay $delay $key
 done
