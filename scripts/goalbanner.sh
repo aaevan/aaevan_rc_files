088c0251fe9ca7b695f95b65b77f738c2792cb84
Aaron Evan-Browning <aaevan@gmail.com>
2019-09-06 23:12:09 -0400

    Made goalbanner.sh less jarring and adjusted fontsize to new screen

--- scripts/goalbanner.sh
+++ scripts/goalbanner.sh
@@ -2,10 +2,12 @@
 echo "What are you trying to work on right now?: "
 read task
 
-for i in `seq 1 750`;
+#for i in `seq 1 750`;
+for i in `seq 1 1500`;
 do
-        echo TASK:$task | osd_cat --pos=bottom --align=center --font=-*-helvetica-bold-r-*-*-30-*-*-*-*-*-*-* --color=#00FF00 --offset=-120 --delay=1 --outline=2
-    sleep 1;
+        echo TASK: $task | osd_cat --pos=bottom --align=center --font=-*-helvetica-bold-r-*-*-20-*-*-*-*-*-*-* --color=#00FF00 --offset=-100 --delay=2 --outline=2
+        echo TASK: $task | osd_cat --pos=bottom --align=center --font=-*-helvetica-bold-r-*-*-20-*-*-*-*-*-*-* --color=#008800 --offset=-100 --delay=2 --outline=2
+    #sleep 1;
 done
 
 
