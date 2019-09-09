37a18a77c7814999f41738e453d49e33bd79bda6
Aaron Evan-Browning <aaevan@gmail.com>
2019-09-09 18:15:11 -0400

    Config files update for Mon Sep  9 18:15:11 EDT 2019

--- scripts/goalbanner.sh
+++ scripts/goalbanner.sh
@@ -5,8 +5,8 @@ read task
 #for i in `seq 1 750`;
 for i in `seq 1 1500`;
 do
-        echo TASK: $task | osd_cat --pos=bottom --align=center --font=-*-helvetica-bold-r-*-*-20-*-*-*-*-*-*-* --color=#00FF00 --offset=-100 --delay=2 --outline=2
-        echo TASK: $task | osd_cat --pos=bottom --align=center --font=-*-helvetica-bold-r-*-*-20-*-*-*-*-*-*-* --color=#008800 --offset=-100 --delay=2 --outline=2
+        echo TASK: $task | osd_cat --pos=bottom --align=center --font=-*-helvetica-bold-r-*-*-20-*-*-*-*-*-*-* --color=#00FF00 --offset=-80 --delay=2 --outline=2
+        echo TASK: $task | osd_cat --pos=bottom --align=center --font=-*-helvetica-bold-r-*-*-20-*-*-*-*-*-*-* --color=#008800 --offset=-80 --delay=2 --outline=2
     #sleep 1;
 done
 
