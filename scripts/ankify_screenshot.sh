5a39d8dd3a38266f2f92b7cecca19e9c9a52808f
Aaron Evan-Browning <aaevan@gmail.com>
2019-11-01 00:04:59 -0400

    Add ankify_screenshot.sh, add keybinding.

---
+++ scripts/ankify_screenshot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+import /home/aaron/ankify/screenshot$(date '+%m-%d-%y_%H-%M-%S').png
+
+#doesn't play nicely with i3? Keeping for backup.
+#gnome-screenshot -a --file=/home/aaron/screens/screenshot$(date '+%m%d-%y_%H-%M-%S').png
+
