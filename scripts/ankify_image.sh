f66653769f784b6d43f8cb5f681b2287b4068c5b
Aaron Evan-Browning <aaevan@gmail.com>
2019-11-21 17:23:12 -0500

    Add hotkey for script to insert most recent screenshot in ankify directory.

---
+++ scripts/ankify_image.sh
@@ -0,0 +1,18 @@
+#f3 --> tab * 9 --> space --> enter
+delay=10
+xdotool keydown --delay $delay Return
+xdotool keyup --delay $delay Return
+xdotool keydown --delay $delay F3
+xdotool keyup --delay $delay F3
+for i in $(seq 1 9)
+do
+    xdotool keydown --delay $delay Tab
+    xdotool keyup --delay $delay Tab
+done
+xdotool keydown --delay $delay space
+xdotool keyup --delay $delay space
+for i in $(seq 1 2)
+do
+    xdotool keydown --delay $delay Return
+    xdotool keyup --delay $delay Return
+done
