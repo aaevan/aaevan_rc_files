c9b966710af17e67400fe7747e0872bbbaf6dfd4
Aaron Evan-Browning <aaevan@gmail.com>
2020-01-20 16:59:57 -0500

    Config files update for Mon Jan 20 16:59:57 EST 2020

---
+++ scripts/anki_vim_clear_macro.sh
@@ -0,0 +1,17 @@
+#system wide macro to delete an entire vim document
+#ggvGdd
+delay=10
+xdotool keydown --delay $delay g
+xdotool keyup --delay $delay g
+xdotool keydown --delay $delay g
+xdotool keyup --delay $delay g
+xdotool keydown --delay $delay v
+xdotool keyup --delay $delay v
+xdotool keydown --delay $delay G
+xdotool keyup --delay $delay G
+xdotool keydown --delay $delay dollar
+xdotool keyup --delay $delay dollar
+xdotool keydown --delay $delay d
+xdotool keyup --delay $delay d
+xdotool keydown --delay $delay d
+xdotool keyup --delay $delay d
---
+++ scripts/stopwatch.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#define default values:
+MINUTES=25
+SECONDS=0
+WILL_BREAK=1
+
+#handle arguments:
+while getopts ":m:s:b:" opt; do
+  case $opt in
+    m)
+      echo "-m was triggered, Parameter: $OPTARG" >&2
+      MINUTES=$OPTARG
+      ;;
+    s)
+      echo "-s was triggered, Parameter: $OPTARG" >&2
+      SECONDS=$OPTARG
+      ;;
+    b)
+      echo "-b was triggered, Parameter: $OPTARG" >&2
+      WILL_BREAK=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+#echo Starting countdown for $MINUTES minutes and $SECONDS seconds.
+if (($SECONDS > 0))
+then
+    for second in `seq -w $SECONDS -1 00`
+    do
+        for ms in `seq -w 99 -1 00`
+        do
+            #echo $MINUTES:$second:$ms
+            printf "%02d:%02d:%02d\n" "${MINUTES#0}" "${second#0}" "${ms#0}"
+            sleep .01
+        done
+        #echo $MINUTES:$second
+        #sleep 1
+    done
+else
+    echo $MINUTES:00:00
+    sleep .01
+fi
+
+for minute in `seq -w $(($MINUTES - 1)) -1 0`
+do
+        for second in `seq -w 59 -1 0`
+        do
+            #echo $minute:$second
+            for ms in `seq -w 9 -1 0`
+            do
+                #echo $minute:$second:$ms
+                printf "%02d:%02d:%d\n" "${minute#0}" "${second#0}" "${ms#0}"
+                sleep .1
+                clear
+            done
+            #sleep 1
+        done
+done
