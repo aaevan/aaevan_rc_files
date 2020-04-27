26a3f6e718bc4c69d87e1acfb79da04676d86150
Aaron Evan-Browning <aaevan@gmail.com>
2020-04-27 10:32:25 -0400

    Fix anki_vim_clear_macro.sh

--- scripts/anki_vim_clear_macro.sh
+++ scripts/anki_vim_clear_macro.sh
@@ -1,9 +1,12 @@
 #clear a vim document:
+#esc esc ggb$G$d esc
 xdotool key Escape
 xdotool key Escape
 xdotool key g
 xdotool key g
 xdotool key v
+xdotool key dollar
 xdotool key G
+xdotool key dollar
 xdotool key d
 xdotool key Escape
