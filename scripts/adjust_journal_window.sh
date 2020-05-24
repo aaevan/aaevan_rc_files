ed9055003e96123b89ce80c0d08b70922b89aa5a
Aaron Evan-Browning <aaevan@gmail.com>
2020-05-23 21:26:16 -0400

    add hotkey to open timer, timestamped note and resize window so timer is on top

---
+++ scripts/adjust_journal_window.sh
@@ -0,0 +1,11 @@
+#adjust the window size to be
+#esc esc ggb$G$d esc
+sleep .5
+echo alt+l
+xdotool key alt+h
+echo alt+Shift_L+k
+xdotool key alt+Shift_L+j
+echo alt+r
+xdotool key alt+r 
+xdotool key j j j j j j j j j j j j j j j j j j j j j
+xdotool key Escape
