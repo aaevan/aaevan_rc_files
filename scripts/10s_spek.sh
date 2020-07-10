e3b6fbdbfe52c004d8ed8014793e77fe1e690d7f
Aaron Evan-Browning <aaevan@gmail.com>
2020-07-09 21:12:42 -0400

    write audio to timestamped file, add osd_cat message to indicate recording

--- scripts/10s_spek.sh
+++ scripts/10s_spek.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-arecord -vv -fdat -d 10 test.wav
-spek test.wav
+big_font=-*-helvetica-bold-r-*-*-100-*-*-*-*-*-*-*
+filename=~/spek_clips/sound_clip_$(date '+%m-%d-%y_%H-%M-%S').wav
+arecord -vv -fdat -d 10 $filename &
+for seconds in `seq -w 10 -1 0`
+do
+    message_text="(recording: $seconds)"
+    echo $message_text | osd_cat --pos=middle --align=center --color=green --font=$big_font --outline=4 --offset=-100 -d 1 &
+    sleep 1
+done
+spek $filename
