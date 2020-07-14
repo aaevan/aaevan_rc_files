6fcbfa82586e9d178d66820c52bce10b8b920739
Aaron Evan-Browning <aaevan@gmail.com>
2020-07-14 15:42:35 -0400

    Config files update for Tue Jul 14 15:42:35 EDT 2020

--- scripts/extract_text_from_area_screen.sh
+++ scripts/extract_text_from_area_screen.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+#take a black and white area screenshot:
 import -monochrome /home/aaron/ankify/screenshot$(date '+%m-%d-%y_%H-%M-%S').png
 
+#take the last file in my screenshots directory and do OCR:
 tesseract ~/ankify/`ls -Art ~/ankify | tail -n 1` out
 
+#open the file in vim:
 gvim ~/out.txt
+
+
+
+
