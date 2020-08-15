cef895d2a25b5e1cba3cf07e4849139d4a5dbc71
Aaron Evan-Browning <aaevan@gmail.com>
2020-08-14 21:58:18 -0400

    fix wakeup.sh

--- scripts/wakeup.sh
+++ scripts/wakeup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-sleep 1;
+sleep 15m;
+sleep 8h;
+echo "playing spotify... (`date '+%H:%M:%S'`)"
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
+echo "raising volume... (`date '+%H:%M:%S'`)"
 for i in `seq -51 .2 0`
 do
-    echo "i is: $i"
-    command="amixer -c 1 -- set PCM ${i}dB"
-    echo "command is: $command"
-    #amixer -c 1 -- set PCM -$i%
-    eval $command
-    sleep 1
+    echo "setting volume to ${i}dB..."
+    amixer -c 1 -- set PCM ${i}dB > /dev/null
+    sleep 2
 done
+echo "finished at `date '+%H:%M:%S'`"
