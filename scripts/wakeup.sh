83bad0b4f91e2cab930e7c8d53f6305fd12b382c
Aaron Evan-Browning <aaevan@gmail.com>
2020-08-27 11:14:56 -0400

    change values in wakeup.sh, update pomodoro_log

--- scripts/wakeup.sh
+++ scripts/wakeup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sleep 15m;
+echo "zeroing out PCM level...";
+amixer -c 1 -- set PCM -51dB > /dev/null
+echo "Read for 30 minutes!";
+sleep 30m;
+echo "Sleeping for 8 hours...";
 sleep 8h;
 echo "playing spotify... (`date '+%H:%M:%S'`)"
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
@@ -8,6 +12,6 @@ for i in `seq -51 .2 0`
 do
     echo "setting volume to ${i}dB..."
     amixer -c 1 -- set PCM ${i}dB > /dev/null
-    sleep 2
+    sleep 3
 done
 echo "finished at `date '+%H:%M:%S'`"
