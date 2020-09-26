cd1b3644d72cf4f45d4cd2f2d845c4aba7ba51b0
Aaron Evan-Browning <aaevan@gmail.com>
2020-09-26 00:51:35 -0400

    Add stonks hotkey

---
+++ scripts/stonks.sh
@@ -0,0 +1,4 @@
+#watch -c -n 1 ./ticker.sh SPY AAPL MSFT GOOG BTC-USD
+
+watch -c -n 1 /home/aaron/scripts/ticker.sh AAPL MSFT GOOG --- BTC-USD ETH-USD --- SPY NDAQ ^DJI ^IXIC --- GLD SLV --- RB=F CL=F --- KC=F RU=F
+
