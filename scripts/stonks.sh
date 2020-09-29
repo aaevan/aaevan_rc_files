db3152c9fc47a0004625d7bc38194514160e029a
Aaron Evan-Browning <aaevan@gmail.com>
2020-09-28 23:40:51 -0400

    add TSLA to stonks.sh

--- scripts/stonks.sh
+++ scripts/stonks.sh
@@ -1,4 +1,4 @@
 #watch -c -n 1 ./ticker.sh SPY AAPL MSFT GOOG BTC-USD
 
-watch -c -n 1 /home/aaron/scripts/ticker.sh AAPL MSFT GOOG --- BTC-USD ETH-USD --- SPY NDAQ ^DJI ^IXIC --- GLD SLV --- RB=F CL=F --- KC=F RU=F
+watch -c -n 1 /home/aaron/scripts/ticker.sh AAPL MSFT GOOG TSLA --- BTC-USD ETH-USD --- SPY NDAQ ^DJI ^IXIC --- GLD SLV --- RB=F CL=F --- KC=F RU=F
 
