1ecfdc7cf0222e64b833040bc87ef7decb91a581
Aaron Evan-Browning <aaevan@gmail.com>
2020-11-23 00:28:30 -0500

    add hotkey for templated timestamped note

--- scripts/templated_timestamped_note.sh
+++ scripts/templated_timestamped_note.sh
@@ -1,4 +1,4 @@
 path=~/notes/note_$(date '+%m-%d-%y_%H-%M-%S').txt
 cp ~/todo_template.txt $path
-echo -e "todos for `date`:\n$(cat $path)" > $path
+echo "todos for `date '+%m-%d-%y (started at %H:%M:%S)'`:\n$(cat $path)" > $path
 gvim $path
