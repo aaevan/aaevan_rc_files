8bf2b611b4d01b633a27a1629bacaf509802437b
Aaron Evan-Browning <aaevan@gmail.com>
2021-02-05 02:25:58 -0500

    15_bells.sh: remove message from aplay, add an extra minute,

--- scripts/15_bells.sh
+++ scripts/15_bells.sh
@@ -1,27 +1,28 @@
 #exercise bell!
 for i in `seq 3`
     do
-    aplay ~/scripts/bell.wav &
+    aplay ~/scripts/bell.wav -q &
     sleep 1
 done
 
 sleep 1
 
-for i in `seq 15`
+for i in `seq 16`
     do
-    aplay ~/scripts/bell.wav &
+        echo "($i)"
+        aplay ~/scripts/bell.wav -q &
     sleep 60
 done
 
 #finish: 3*.1, 3*.1
 for i in `seq 3`
     do
-    aplay ~/scripts/bell.wav &
+    aplay ~/scripts/bell.wav -q &
     sleep .12
 done
 sleep 1
 for i in `seq 3`
     do
-    aplay ~/scripts/bell.wav &
+    aplay ~/scripts/bell.wav -q &
     sleep .12
 done
