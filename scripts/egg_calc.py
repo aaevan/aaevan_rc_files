155c0ea22d1d6fb69449498d37ae2e157fe271c5
Aaron Evan-Browning <aaevan@gmail.com>
2021-02-27 21:54:46 -0500

    make egg_calc use pretty formatting

--- scripts/egg_calc.py
+++ scripts/egg_calc.py
@@ -1,5 +1,20 @@
 from math import sqrt
 from time import sleep
+import sys
+
+# Finds optimal arrangements of eggs in a 2x6 carton 
+# such that the combined center of gravity will be as close
+# as possible to the perceived center of gravity.
+
+# turns out there's a lot of combinations that work equally fine:
+# 3  eggs, 48 places tied for first
+# 5  eggs, 144 places tied for first
+# 7  eggs, 144 places tied for first
+# 9  eggs, 48 places tied for first
+# 11 eggs, 4 places tied for first
+
+# more average mass of egg closer to the center point
+# means less of a surprise when picking up the carton.
 
 # configurations are given as a list of coordinates:
 # [(0, 1), (1, 1), (2, 1)]3
@@ -12,9 +27,7 @@ from time import sleep
 # position 0 of the string is position (0, 1), position 11 is 
 # position (5, 0)
 
-
 def avg_xy(arr):
-    print(arr)
     x_sum, y_sum = 0, 0
     for coord in arr:
         x_sum += coord[0]
@@ -31,7 +44,6 @@ def sum_of_dists(arr=[(0, 0)], center=(2.5, .5), ):
     return dist_sum
 
 def point_to_point_dist(coord_a, coord_b):
-    print(f"input: coord_a:{coord_a}, coord_b:{coord_b}")
     x_diff, y_diff = (
         coord_b[0] - coord_a[0],
         coord_b[1] - coord_a[1],
@@ -53,42 +65,49 @@ def bin_str_to_coords(bin_str, coord_dict):
             output_list.append(coord_dict[index])
     return output_list
 
-def bin_str_to_pretty_output(bin_str, line_length=6):
+
+def bin_str_to_pretty_output(bin_str='010101101010', row=(), line_length=6):
+    center_dist, sum_dists, bin_perm, egg_coords = row
     output_list = []
     for index, char in enumerate(bin_str):
         if char == '1':
             output_list.append('●')
         else:
             output_list.append('○')
-        if index == 5:
-            output_list.append('  dist from center:    sum_dists:    permutation:   egg_coords:    \n')
-    return ''.join(output_list)
+    str_output = ''.join(output_list)
+    first_egg_row, second_egg_row = str_output[:6], str_output[6:]
+    line_1 = f'{first_egg_row}  center_dist   sum_dists     bin_perm      egg_coords'
+    formatted_row = "{0:<14}{1:<14}{2:<14}{3:<14}".format(
+        round(center_dist, 8),
+        round(sum_dists, 8), 
+        bin_perm,
+        str(egg_coords),
+    )
+    line_2 = f'{second_egg_row}  {formatted_row}'
+    print(line_1)
+    print(line_2)
 
 def main():
+    if len(sys.argv) > 1:
+        count_calc = int(sys.argv[1])
+    else:
+        count_calc = 3
     coord_dict = {
         0: (0,1), 1: (1,1), 2: (2,1), 3: (3,1), 4: (4,1), 5: (5,1),
         6: (0,0), 7: (1,0), 8: (2,0), 9: (3,0), 10: (4,0), 11: (5,0),
     }
-    egg_permutations = get_egg_permutations(count = 3)
+    egg_permutations = get_egg_permutations(count = count_calc)
     center = (2.5, .5)
-    print(egg_permutations)
-    print(egg_permutations[0])
-    min_dist = 999
-    min_perm = None
     egg_dists = []
     for bin_str in egg_permutations:
         coord_list = bin_str_to_coords(bin_str=bin_str, coord_dict=coord_dict)
-        #print(f"coord_list is: {coord_list}")
-        avg_egg_coord = avg_xy(bin_str_to_coords(bin_str=bin_str, coord_dict=coord_dict))
-        #print(f"avg_egg_coord is: {avg_egg_coord}")
+        avg_egg_coord = avg_xy(
+            bin_str_to_coords(bin_str=bin_str, coord_dict=coord_dict)
+        )
         dist_from_center = point_to_point_dist(center, avg_egg_coord)
-        sum_of_dists_from_center = sum_of_dists(arr=coord_list, center=center, )
-        #print(f"sum_of_dists_from_center is: {sum_of_dists_from_center}")
-        if dist_from_center <= min_dist:
-            min_dist = dist_from_center
-            min_perm = (coord_list, dist_from_center)
-        #print(f"dist_from_center is: {dist_from_center}")
-        print(bin_str_to_pretty_output(bin_str=bin_str))
+        sum_of_dists_from_center = sum_of_dists(
+            arr=coord_list, center=center, 
+        )
         egg_dists.append(
             (
                 round(dist_from_center, 16),
@@ -97,15 +116,10 @@ def main():
                 coord_list
             )
         )
-    print(min_dist, min_perm)
-    #print(egg_dists)
-    print(egg_dists[0])
     for row_num, row in enumerate(sorted(egg_dists, reverse=True)):
-        print(len(egg_dists) - row_num, "-------------------------------------------------------------------")
-        print(bin_str_to_pretty_output(row[2]), row)
-    print("---------------------------------------------------------------------")
-
-
+        print( len(egg_dists) - row_num, '-' * 67,)
+        bin_str_to_pretty_output(bin_str=row[2], row=row)
+    print('-' * 69)
 
 main()
 
