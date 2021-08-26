7c21a62acb939501e2b12d86b6038c14fdad0ab0
Aaron Evan-Browning <aaevan@gmail.com>
2021-08-25 21:04:13 -0400

    fix list_pomodoros.sh to correctly grep through pomodoro_log.txt

--- scripts/list_pomodoros.sh
+++ scripts/list_pomodoros.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #count=$(grep -c `date --rfc-3339='date'` ~/pomodoro_log.txt)
-count=$(grep -c "`date "+%c"`" ~/pomodoro_log.txt)
+count=$(grep -c "`date "+%c" | cut -c -15`" ~/pomodoro_log.txt)
 echo "$count pomodoros from today:"
 
 #cat ~/pomodoro_log.txt | grep "`date "+%c"`"
