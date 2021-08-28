186e85b75a4fbe49ca90be98070634124c5111b6
Aaron Evan-Browning <aaevan@gmail.com>
2021-08-27 20:56:15 -0400

    more work on backslash_osd_arrow.sh: drag box corners coords properly offset

--- scripts/backslash_osd_arrow.sh
+++ scripts/backslash_osd_arrow.sh
@@ -6,11 +6,28 @@
 #tr "x" " " replaces instances of x with a space
 read -r width height screenx screeny x1 y1 <<<$(import -identify /home/aaron/screens/screenshot$(date '+%m-%d-%y_%H-%M-%S').png | awk '{print $3 " " $4}' | tr "x" " " | tr "+" " ")
 
-x2=$(($x+$width))
-y2=$(($y+$height))
+x_offset=19
+y_offset=45
 
-echo screenx:$screenx screeny:$screeny width:$width height:$height x1:$x1 y1:$y1 x2:$x2 y2:$y2
+x2=$(($screenx-$x1))
+y2=$(($screeny-$y1))
 
-#does weird bullshit with multiple screens. gotta figure out better absolute coordinates with osd_cat?
-echo . | osd_cat --pos=top --align=right --font=-*-helvetica-bold-r-*-*-60-*-*-*-*-*-*-* --offset=$y1 -i $x1 -d 60 --color="#ff0000" &
-echo . | osd_cat --pos=top --align=right --font=-*-helvetica-bold-r-*-*-60-*-*-*-*-*-*-* --offset=$y2 -i $x2 -d 60 --color="#00ff00" &
+x3=$(($x2-$x_offset))
+y3=$(($y1-$y_offset))
+
+x4=$(($x3-$width))
+y4=$(($y3+$height))
+
+
+echo screenx:$screenx screeny:$screeny width:$width height:$height 
+echo x1:$x1 y1:$y1 x2:$x2 y2:$y2 x3:$x3 y3:$y3
+
+#does weird bullshit with multiple screens.
+#I've gotta figure out how to get absolute coordinates with multiple screens 
+# and translate that to what osd_cat uses.
+
+echo \# | osd_cat --pos=top --align=right --font=-*-helvetica-bold-r-*-*-60-*-*-*-*-*-*-* --offset=$y1 -i $x1 -d 60 --color="#ff0000" &
+echo \# | osd_cat --pos=top --align=right --font=-*-helvetica-bold-r-*-*-60-*-*-*-*-*-*-* --offset=$y2 -i $x2 -d 60 --color="#00ff00" &
+echo . | osd_cat --pos=top --align=right --font=-*-helvetica-bold-r-*-*-60-*-*-*-*-*-*-* --offset=$y3 -i $x3 -d 60 --color="#00ffff" &
+echo . | osd_cat --pos=top --align=right --font=-*-helvetica-bold-r-*-*-60-*-*-*-*-*-*-* --offset=$y4 -i $x4 -d 60 --color="#ff00ff" &
+#echo \# | osd_cat --pos=top --align=right --font=-*-helvetica-bold-r-*-*-60-*-*-*-*-*-*-* --offset=$y5 -i $x5 -d 60 --color="#ffff00" &
