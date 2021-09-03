24016069adb36d844d071c492c71d037016a8b0c
Aaron Evan-Browning <aaevan@gmail.com>
2021-09-02 23:26:10 -0400

    add points_between_points.py

---
+++ scripts/points_between_points.py
@@ -0,0 +1,29 @@
+from sys import argv
+import numpy as np
+from math import sqrt
+
+x1, y1, x2, y2 = [int(val) for val in argv[1:5]]
+debug = False
+
+if debug:
+    print(f'x1 is: {x1}, y1 is: {y1} x2 is: {x2}, y2 is: {y2}')
+
+dist = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+spacing = 15 #pixels
+num_points = dist // spacing
+
+points = [
+    *zip(
+        np.linspace(x1, y1, num=num_points), 
+        np.linspace(x2, y2, num=num_points), 
+    )
+]
+
+rounded_points = [(int(x), int(y)) for x, y in points]
+
+if debug:
+    print("rounded_points:")
+    print(rounded_points)
+
+for x, y in rounded_points:
+    print(f'{x} {y}')
