7218b9402fd2601ab5aade999adb80953271e8be
Aaron Evan-Browning <aaevan@gmail.com>
2021-10-27 00:06:38 -0400

    clean up rand_10s_pause.sh

--- scripts/rand_10s_pause.sh
+++ scripts/rand_10s_pause.sh
@@ -1,17 +1,14 @@
-monitor_state="`xset q | grep Monitor`"
-echo "monitor_state is: $monitor_state"
+#!/bin/bash
 
-aplay ~/scripts/bell.wav -q &
+monitor_state="`xset q | grep Monitor`"
 
 if [[ "$monitor_state" == *"is On"* ]]; then
     rand_val=$RANDOM
-    one_in_32=$(($(($rand_val<2000))==1))
-
-    echo "rand_val is $rand_val."
-    echo "one_in_32 is $one_in_32."
-
+    #the max val for $RANDOM is 32000, so a value of 1000 is a 1:32 chance
+    one_in_32=$(($(($rand_val<1000))==1))
     if [ $one_in_32 == 1 ];
         then
+        echo "take a short break and think!"
         aplay ~/scripts/chime.wav -q &
         sleep 40
         killall aplay
--- scripts/rand_pause_wrapper.sh
+++ scripts/rand_pause_wrapper.sh
@@ -2,5 +2,4 @@
 
 #add the following (uncommented) to roots crontab (sudo crontab -e):
 #* * * * * su aaron -c "DISPLAY=:0.0 /home/aaron/scripts/rand_pause_wrapper.sh             
-
 xfce4-terminal --hide-menubar -e /home/aaron/scripts/rand_10s_pause.sh
