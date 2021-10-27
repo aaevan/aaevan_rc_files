88db1d92f9af94608cb6ef2d9111b61537ea04d3
Aaron Evan-Browning <aaevan@gmail.com>
2021-10-27 12:12:43 -0400

    fix rand_10s_pause scripts to be less noisy

--- scripts/rand_10s_pause.sh
+++ scripts/rand_10s_pause.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-monitor_state="`xset q | grep Monitor`"
+#TODO: write a wrapper that can be applied to any script and
+#      passed into crontab, accept parameters (script and chance)
 
-if [[ "$monitor_state" == *"is On"* ]]; then
-    rand_val=$RANDOM
-    #the max val for $RANDOM is 32000, so a value of 1000 is a 1:32 chance
-    one_in_32=$(($(($rand_val<1000))==1))
-    if [ $one_in_32 == 1 ];
-        then
-        echo "take a short break and think!"
-        aplay ~/scripts/chime.wav -q &
-        sleep 40
-        killall aplay
-    fi
-fi
-
+echo "take a short break and think!"
+aplay ~/scripts/chime.wav -q &
+sleep 40
+killall aplay
--- scripts/rand_pause_wrapper.sh
+++ scripts/rand_pause_wrapper.sh
@@ -2,4 +2,16 @@
 
 #add the following (uncommented) to roots crontab (sudo crontab -e):
 #* * * * * su aaron -c "DISPLAY=:0.0 /home/aaron/scripts/rand_pause_wrapper.sh             
-xfce4-terminal --hide-menubar -e /home/aaron/scripts/rand_10s_pause.sh
+
+monitor_state="`xset q | grep Monitor`"
+
+if [[ "$monitor_state" == *"is On"* ]]; then
+    rand_val=$RANDOM
+    #the max val for $RANDOM is 32000, so a value of 1000 is a 1:32 chance
+    one_in_32=$(($(($rand_val<1000))==1))
+    if [ $one_in_32 == 1 ];
+        then
+        xfce4-terminal --hide-menubar -e /home/aaron/scripts/rand_10s_pause.sh
+    fi
+fi
+
