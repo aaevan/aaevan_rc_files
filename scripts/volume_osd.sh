5301da6b26d6a5ae1d7006dce1d6f07ae96d827c
Aaron Evan-Browning <aaevan@gmail.com>
2021-12-30 13:25:13 -0500

    volume_osd.sh: reposition and trim to just one bar

--- scripts/volume_osd.sh
+++ scripts/volume_osd.sh
@@ -3,8 +3,10 @@ right=`amixer -D pulse get Master | grep Right`
 left_percent=`echo $left | cut -d "[" -f2 | cut -d "%" -f1`
 right_percent=`echo $right | cut -d "[" -f2 | cut -d "%" -f1`
 
-osd_cat -o 5 -i 50 -d 1 --barmode=percentage -P $left_percent -c green &
-osd_cat -o 20 -i 50 -d 1 --barmode=percentage -P $right_percent -c green &
+killall osd_cat
+osd_cat -o 3 -i 280 -d 1 --barmode=percentage -P $left_percent -c green &
+#osd_cat -o 20 -i 50 -d 1 --barmode=percentage -P $right_percent -c green &
 
-echo $left_percent | osd_cat -o 40 -i 50 -d 1 -c green &
+echo $left_percent% | osd_cat -o 1 -i 1570 -d 1 -c green &
+#echo R:$right_percent | osd_cat -o 18 -i 1340 -d 1 -c green &
 
