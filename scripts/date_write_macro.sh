7af9358e5171978a0ea4054f03b631f12f54f65e
Aaron Evan-Browning <aaevan@gmail.com>
2022-02-07 14:15:39 -0500

    add date_write_macro.sh

---
+++ scripts/date_write_macro.sh
@@ -0,0 +1,9 @@
+#from inside a vim document, type out a new file with the current timestamp
+
+# pause here to debounce input so we don't fire off a litany of other commands 
+# with mod4 and shift still held down (not doing this causes hilarious problems)
+sleep 1
+
+command_str=":w note_$(date '+%m-%d-%y_%H-%M-%S').txt"
+
+xdotool type "$command_str"
