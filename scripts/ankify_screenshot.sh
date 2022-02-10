9f1ef391f99f830307cc74751ea4cdc519d4fd94
Aaron Evan-Browning <aaevan@gmail.com>
2022-02-09 23:28:13 -0500

    update a few hotkeys

--- scripts/ankify_screenshot.sh
+++ scripts/ankify_screenshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-import /home/aaron/ankify/screenshot$(date '+%m-%d-%y_%H-%M-%S').png
+import /home/aaron/ankify/screenshot_$(date '+%m-%d-%y_%H-%M-%S').png
 
 #doesn't play nicely with i3? Keeping for backup.
 #gnome-screenshot -a --file=/home/aaron/screens/screenshot$(date '+%m%d-%y_%H-%M-%S').png
