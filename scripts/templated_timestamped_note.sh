c2d1f3b079306389797c46d842420071341f1994
Aaron Evan-Browning <aaevan@gmail.com>
2023-02-26 22:53:11 -0500

    add hotkey for triple screen layout (xrandr command output from arandr)

--- scripts/templated_timestamped_note.sh
+++ scripts/templated_timestamped_note.sh
@@ -1,4 +1,10 @@
-path=~/notes/note_$(date '+%m-%d-%y_%H-%M-%S').txt
+#epoch seconds then date (easier to sort)
+path=~/notes/note_$(date '+%s_%m-%d-%y_%H-%M-%S').txt
+#simple date
+#path=~/notes/note_$(date '+%m-%d-%y_%H-%M-%S').txt
 cp ~/todo_template.txt $path
+#date prefaced by seconds since 1970
 echo "todos for `date '+%m-%d-%y (started at %H:%M:%S)'`:\n$(cat $path)" > $path
+#just the date, harder to sort
+#echo "todos for `date '+%m-%d-%y (started at %H:%M:%S)'`:\n$(cat $path)" > $path
 gvim $path
